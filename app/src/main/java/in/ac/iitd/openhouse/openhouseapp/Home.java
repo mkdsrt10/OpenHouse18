107822ff5500499ba3c5f747f2be69cc03153d60
Sachin622 <kumawat.sachin622>
2018-03-18 16:55:32 +0530

    added new fragment

---
+++ app/src/main/java/in/ac/iitd/openhouse/openhouseapp/Home.java
@@ -0,0 +1,69 @@
+package in.ac.iitd.openhouse.openhouseapp;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Created by mayankdubey on 17/03/18.
+ */
+
+public class projects extends Fragment   {
+
+
+
+
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+
+        View view = inflater.inflate(R.layout.projects_fragment, container, false);
+
+
+
+
+
+
+        return view;
+
+
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        //you can set the title for your toolbar here for different fragments different titles
+        getActivity().setTitle("Home");
+
+    }
+
+    
+
+
+}
+
